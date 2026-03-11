05d2a2e396dc0fb480bf0b1584fc07e8d68d1eb6
gongueiro1 <ggoncalosilva1@gmail.com>
2026-03-11 09:46:25 +0000

    Adicionar projeto API

---
+++ API/API/Data/ApplicationDbContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options){
+}
