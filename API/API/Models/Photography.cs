4edfe00b911b414fd7255bde813ff9ab45143545
gongueiro1 <ggoncalosilva1@gmail.com>
2026-03-11 10:11:50 +0000

    classes iniciais do model

---
+++ API/API/Models/Photography.cs
@@ -0,0 +1,15 @@
+namespace API.Models;
+
+public class Photography{
+    public int Id { get; set; }
+    
+    public String Title { get; set; }
+    
+    public String Description { get; set; }
+    
+    public String File { get; set; }
+    
+    public DateTime Date { get; set; }
+    
+    public decimal Price { get; set; }
+}
